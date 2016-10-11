2f47e5f44ee862f1212c669363461ed8d47a8659
chankyu oh <chankyuoh@u.northwestern.edu>
2016-10-11 22:41:01 +0800

    fix error message in AddCommand.java to match startDate changes

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -18,7 +18,7 @@ public class AddCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the scheduler. "
             + "Parameters: NAME sd/STARTDATE ed/ENDDATE a/ADDRESS  [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
-            + " John Doe p/98765432 ed/24-10-2016 a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
+            + " meet group sd/25-10-2016 ed/24-10-2016 a/311, Clementi Ave 2, #02-25 t/important t/school";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the scheduler";
