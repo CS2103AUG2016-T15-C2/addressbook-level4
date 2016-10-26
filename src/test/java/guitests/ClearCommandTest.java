9483fb8ffe4bc8787042c9042521e2ca6e320cee
chankyu oh <chankyuoh@u.northwestern.edu>
2016-10-26 22:35:04 +0800

    fix clear bug

--- src/test/java/guitests/ClearCommandTest.java
+++ src/test/java/guitests/ClearCommandTest.java
@@ -12,12 +12,12 @@ public class ClearCommandTest extends SchedulerGuiTest {
     public void clear() {
 
         //verify a non-empty list can be cleared using advanced command
-        //assertTrue(entryListPanel.isListMatching(td.getTypicalEntrys()));
+        assertTrue(entryListPanel.isListMatching(td.getTypicalEntrys()));
         assertAdvancedClearCommandSuccess();
 
         //verify other commands can work after a clear command
         commandBox.runCommand(td.meeting.getAddCommand());
-        //assertTrue(entryListPanel.isListMatching(td.meeting));
+        assertTrue(entryListPanel.isListMatching(td.meeting));
         commandBox.runCommand("delete 1");
         assertListSize(0);
         //verify clear command works when the list is empty
