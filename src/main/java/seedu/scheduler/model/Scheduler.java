b5597c44f782570af3ab4ed8f432f6cbe98032c0
Lam Jouq Hang Hendersen <hendersenlam@gmail.com>
2016-10-26 18:54:03 +0800

    Add comment to Scheduler.java

--- src/main/java/seedu/scheduler/model/Scheduler.java
+++ src/main/java/seedu/scheduler/model/Scheduler.java
@@ -112,6 +112,14 @@ public class Scheduler implements ReadOnlyScheduler {
         }
     }
     
+    /**
+     * Edits(replace) a specified entry in the list.
+     * 
+     * @throws UniqueEntryList.DuplicateEntryException if an equivalent entry already exists.
+     * @throws UniqueEntryList.EntryNotFoundException if no such entry could be found in the list.
+     * 
+     * @@author A0152962B
+     */
     public void editEntry(int index, Entry r, ReadOnlyEntry toEdit) throws UniqueEntryList.DuplicateEntryException, UniqueEntryList.EntryNotFoundException {
         entrys.edit(index, r, toEdit);
     }
